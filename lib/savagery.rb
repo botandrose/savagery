d6b727c17843fc9a80187b572da8dd29f3129be0
Micah Geisel <micah@botandrose.com>
2014-11-11 17:39:29 -0800

    tdd basic concept.

--- lib/savagery.rb
+++ lib/savagery.rb
@@ -5,11 +5,67 @@ module Savagery
     Spriter.new(path).sprite!
   end
 
-  class Spriter
-    def initialize path
+  class Spriter < Struct.new(:path)
+    def sprite!
+      File.write sprite_path, sprite
     end
 
-    def sprite!
+    private
+
+    def sprite
+      header + images + footer
+    end
+
+    def sprite_path
+      path + ".svg"
+    end
+
+    def header
+      "<svg><defs>\n\n"
+    end
+
+    def images
+      Image.from_paths(image_paths).map(&:symbol).join("\n")
+    end
+
+    def footer
+      "\n</defs></svg>\n"
+    end
+
+    def image_paths
+      Dir.glob(path + "/*.svg").sort
+    end
+
+    class Image < Struct.new(:path)
+      def self.from_paths paths
+        paths.map { |path| Image.new(path) }
+      end
+
+      def symbol
+        header + image + footer
+      end
+
+      private
+      
+      def header
+        %(<symbol id="#{name}">)
+      end
+
+      def image
+        data[/<svg\b[^>]*>(.+)<\/svg>/m, 1].gsub(/[\r\n]+/, "\n")
+      end
+
+      def footer
+        "</symbol>\n"
+      end
+
+      def data
+        File.read(path)
+      end
+
+      def name
+        File.basename(path, ".svg")
+      end
     end
   end
 end
--- spec/savagery_spec.rb
+++ spec/savagery_spec.rb
@@ -1,16 +1,19 @@
 require "savagery"
 require "fileutils"
+require "byebug"
 
 describe Savagery do
   before do
     FileUtils.rm_rf "tmp/svgs"
+    FileUtils.rm_rf "tmp/svgs.svg"
     FileUtils.cp_r "spec/support/svgs", "tmp/svgs"
   end
 
   it ".sprite!" do
     Savagery.sprite!("tmp/svgs")
-    File.read("tmp/svgs.svg").should == """
-    """
+    actual = File.read("tmp/svgs.svg")
+    expected = File.read("spec/support/svgs.svg")
+    expect(actual).to eq expected
   end
 end
 
